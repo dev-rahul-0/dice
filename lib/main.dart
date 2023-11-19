1399f72d047e4f840cb265649c0f75f24c43929c
dev-rahul-0 <rs650417@gmail.com>
2023-11-19 14:07:14 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import 'gradient_container.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: GradientContainer(
+          Color.fromARGB(255,33,5,109),
+          Color.fromARGB(255,68,21,149)
+        ),
+      ),
+    );
+  }
+}
